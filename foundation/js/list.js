0261dfc3117d5f9c959ad6cba141c78bd0638da6
hrisanthi <hrisanthi.kroi@gmail.com>
2014-12-01 20:10:31 -0500

    fing up the pic

--- foundation/js/list.js
+++ foundation/js/list.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     initialize();
+    choosePhoto();
+    okay();
     
      
     
@@ -108,5 +110,39 @@ function getList(UNCBookShare){
         }
         
     });
+    
+    function choosePhoto(){
+        //alert("capture button working");
+    navigator.camera.getPicture(gotPic,failHandler,{sourceType:0, destinationType:0, quality:10});
+}  
+ 
+	
+	function gotPic(data) {
+		console.log('got here');
+		imagedata = data;
+		cameraPic.src = "data:image/jpeg;base64," + data;
+		//$("#takePicBtn").text("Picture Taken!").button("refresh");
+	}
+	
+	function failHandler(e) {
+		alert("ErrorFromC");
+		alert(e);
+		console.log(e.toString());
+	}
+ 
+	function cleanUp() {
+		imagedata = "";
+		//$("#submitBtn").removeAttr("disabled").button("refresh");
+		$("#caption").val("");
+		//$("#takePicBtn").text("Add Pic").button("refresh");
+	}
+ 
+//});
+
+ 
+
+
+function okay(message) {
+}
 }
 
